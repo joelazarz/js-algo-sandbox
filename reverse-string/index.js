bc0c60cd7887e8b58283e9474c606459c10654ac
joe lazarz <jlazarz87@gmail.com>
2020-01-29 16:15:37 -0500

    reverse-string

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -1,11 +1,49 @@
 
 
-function reverse(string) {
-  let splitString = string.split('');
-  let reverseArray = splitString.reverse();
-  let joinedArray = reverseArray.join('');
-
-  return joinedArray; 
+function reverse(str) {
+  let arr = str.split('');
+  arr.reverse();
+  return arr.join(''); // olleh
 };
 
+
+/////
+function reverseTwo(str) {
+  return str
+  .split('')
+  .reverse()
+  .join(''); // olleh
+};
+
+
+/////
+function reverseThree(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  };
+  
+  return reversed; // olleh
+};
+
+/////
+function reverseFour(str) {
+  return Array.from(String(str)).reverse().join('');  // olleh
+};
+
+/////
+function reverseFive(str) {
+  str.split('').reduce((rev, char) => char + rev, '');  // olleh
+};
+
+// reverse('hello')
+// reverseTwo('hello')
+// reverseThree('hello')
+// reverseFour('hello')
+// reverseFive('hello')
+
 module.exports = reverse;
+module.exports = reverseTwo;
+module.exports = reverseThree;
+module.exports = reverseFour;
+module.exports = reverseFive;
--- reverse-string/test.js
+++ reverse-string/test.js
@@ -1,4 +1,8 @@
 const reverse = require('./index');
+const reverseTwo = require('./index');
+const reverseThree = require('./index');
+const reverseFour = require('./index');
+const reverseFive = require('./index');
 
 test('Reverse function exists', () => {
   expect(reverse).toBeDefined();
@@ -10,4 +14,52 @@ test('Reverse reverses a string', () => {
 
 test('Reverse reverses a string', () => {
   expect(reverse('  abcd')).toEqual('dcba  ');
+});
+
+test('Reverse function exists', () => {
+  expect(reverseTwo).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseTwo('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseTwo('  abcd')).toEqual('dcba  ');
+});
+
+test('Reverse function exists', () => {
+  expect(reverseThree).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseThree('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseThree('  abcd')).toEqual('dcba  ');
+});
+
+test('Reverse function exists', () => {
+  expect(reverseFour).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseFour('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseFour('  abcd')).toEqual('dcba  ');
+});
+
+test('Reverse function exists', () => {
+  expect(reverseFive).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseFive('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+  expect(reverseFive('  abcd')).toEqual('dcba  ');
 });
