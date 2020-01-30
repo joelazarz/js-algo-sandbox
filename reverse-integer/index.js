a6dd1540d79807235700f8eccbf758e695ad4a7e
joe lazarz <jlazarz87@gmail.com>
2020-01-30 07:55:08 -0500

    reverse-integer

--- reverse-integer/index.js
+++ reverse-integer/index.js
@@ -1,6 +1,30 @@
 
+// let int = -23
 function reverseInt(int) {
-  return parseInt(Array.from(String(int)).reverse().join('')) * Math.sign(int);
+  return parseInt(Array.from(String(int)).reverse().join('')) * Math.sign(int);   // -32
 }
 
-module.exports = reverseInt;
+
+// let int = -23
+function reverseIntTwo(int) {
+  return parseInt(int.toString().split('').reverse().join('')) * Math.sign(int)   // -32
+}
+
+
+// easier to understand
+// let int = -23
+function reverseIntThree(int) {
+  const reversed = int
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(int);       // -32
+}
+
+
+
+module.exports = reverseInt; 
+module.exports = reverseIntTwo; 
+module.exports = reverseIntThree; 
--- reverse-integer/test.js
+++ reverse-integer/test.js
@@ -1,5 +1,8 @@
 const reverseInt = require('./index');
+const reverseIntTwo = require('./index');
+const reverseIntThree = require('./index');
 
+///////
 test('ReverseInt function exists', () => {
   expect(reverseInt).toBeDefined();
 });
@@ -20,4 +23,51 @@ test('ReverseInt flips a negative number', () => {
   expect(reverseInt(-15)).toEqual(-51);
   expect(reverseInt(-90)).toEqual(-9);
   expect(reverseInt(-2359)).toEqual(-9532);
+});
+
+////////
+test('ReverseInt function exists', () => {
+  expect(reverseIntTwo).toBeDefined();
+});
+
+test('ReverseInt handles 0 as an input', () => {
+  expect(reverseIntTwo(0)).toEqual(0);
+});
+
+test('ReverseInt flips a positive number', () => {
+  expect(reverseIntTwo(5)).toEqual(5);
+  expect(reverseIntTwo(15)).toEqual(51);
+  expect(reverseIntTwo(90)).toEqual(9);
+  expect(reverseIntTwo(2359)).toEqual(9532);
+});
+
+test('ReverseInt flips a negative number', () => {
+  expect(reverseIntTwo(-5)).toEqual(-5);
+  expect(reverseIntTwo(-15)).toEqual(-51);
+  expect(reverseIntTwo(-90)).toEqual(-9);
+  expect(reverseIntTwo(-2359)).toEqual(-9532);
+});
+
+
+////////
+test('ReverseInt function exists', () => {
+  expect(reverseIntThree).toBeDefined();
+});
+
+test('ReverseInt handles 0 as an input', () => {
+  expect(reverseIntThree(0)).toEqual(0);
+});
+
+test('ReverseInt flips a positive number', () => {
+  expect(reverseIntThree(5)).toEqual(5);
+  expect(reverseIntThree(15)).toEqual(51);
+  expect(reverseIntThree(90)).toEqual(9);
+  expect(reverseIntThree(2359)).toEqual(9532);
+});
+
+test('ReverseInt flips a negative number', () => {
+  expect(reverseIntThree(-5)).toEqual(-5);
+  expect(reverseIntThree(-15)).toEqual(-51);
+  expect(reverseIntThree(-90)).toEqual(-9);
+  expect(reverseIntThree(-2359)).toEqual(-9532);
 });
