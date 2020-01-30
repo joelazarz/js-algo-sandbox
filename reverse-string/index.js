d436c51b7ca4b1325a44c770a53b1a5d79788cb0
joe lazarz <jlazarz87@gmail.com>
2020-01-29 20:52:13 -0500

    reverse-string comments

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -1,40 +1,47 @@
 
 
+///// let str = 'hello'
 function reverse(str) {
-  let arr = str.split('');
-  arr.reverse();
-  return arr.join(''); // olleh
+  let arr = str.split('');    // [h,e,l,l,o]
+  arr.reverse();              // [o,l,l,e,h]
+  return arr.join('');        // olleh
 };
 
 
-/////
+/////   let str = 'hello'
 function reverseTwo(str) {
   return str
-  .split('')
-  .reverse()
-  .join(''); // olleh
+  .split('')      // [h,e,l,l,o]
+  .reverse()      // [o,l,l,e,h]
+  .join('');      // olleh
 };
 
 
-/////
+/////   let str = 'hello'
 function reverseThree(str) {
-  let reversed = '';
+  let reversed = '';                      //
   for (let character of str) {
-    reversed = character + reversed;
+    reversed = character + reversed;      // h, eh, leh, lleh, olleh    
   };
   
-  return reversed; // olleh
+  return reversed;                        // olleh
 };
 
-/////
+
+
+/////   let str = 'hello'
 function reverseFour(str) {
   return Array.from(String(str)).reverse().join('');  // olleh
 };
 
-/////
+
+
+/////    let str = 'hello'
 function reverseFive(str) {
   str.split('').reduce((rev, char) => char + rev, '');  // olleh
 };
+
+
 
 // reverse('hello')
 // reverseTwo('hello')
