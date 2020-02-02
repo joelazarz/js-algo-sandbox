2498fe94c1ce0be5d66e14c95c29effdf1aa31a3
joe lazarz <jlazarz87@gmail.com>
2020-02-02 15:04:30 -0500

    fizzbuzz

---
+++ fizbuzz/index.js
@@ -0,0 +1,20 @@
+// console logs the numbers from 1 to n 
+// for multiples of three "fizz" is logged
+// for multiples of five "buzz" is logged
+// for multipes of three and five "fizzbuzz" is logged
+
+function fizzbuzz(n) {
+  for(i=1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  };
+}
+
+module.exports = fizzbuzz;
