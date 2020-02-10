4fda8dc598c54bad178877e469aef66b1c083298
joe lazarz <jlazarz87@gmail.com>
2020-02-10 09:20:59 -0500

    anagrams

--- array-chunking/index.js
+++ array-chunking/index.js
@@ -8,6 +8,8 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+
+//////
 function chunk(array,size) {
   const chunked = [];
 
@@ -25,6 +27,8 @@ function chunk(array,size) {
   return chunked;
 };
 
+
+//////
 function chunkTwo(array,size) {
   const chunked = [];
   let index = 0;
